815352de19ee7eca3154a64bf2e6a8fc5c91eb39
William Baierle <w.baierle@gmail.com>
2022-06-21 19:53:14 -0300

    feat: sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,1 +1,57 @@
-// To do build component
+import React from "react";
+import { useState } from "react";
+import { Container, Content, Logo } from "./style";
+import { BsGraphUp } from "react-icons/bs";
+import { HiUserGroup } from "react-icons/hi";
+import { FaReact } from "react-icons/fa";
+import { FiLogOut } from "react-icons/fi";
+import { useHistory } from "react-router-dom";
+
+export default function Sidebar() {
+  const [Extends, setExtends] = useState(false);
+  const history = useHistory();
+
+  const handleExtend = () => {
+    setExtends(!Extends);
+  };
+
+  const goTo = (location) => {
+    history.push(location);
+  };
+
+  return (
+    <Container Extends={Extends}>
+      <Logo onClick={handleExtend}>
+        <h1>
+          {Extends ? (
+            <>
+              Quality<span>Life</span>
+            </>
+          ) : (
+            <>
+              Q<span>L</span>
+            </>
+          )}
+        </h1>
+      </Logo>
+      <Content Extends={Extends}>
+        <li onClick={() => goTo("/dashboard")}>
+          <BsGraphUp />
+          {Extends ? <h2>Dashboard</h2> : <></>}
+        </li>
+        <li onClick={() => goTo("/groups")}>
+          <HiUserGroup />
+          {Extends ? <h2>Grupos</h2> : <></>}
+        </li>
+        <li onClick={() => goTo("/aboutUs")}>
+          <FaReact />
+          {Extends ? <h2>Sobre Nós</h2> : <></>}
+        </li>
+        <li onClick={() => goTo("/login")}>
+          <FiLogOut />
+          {Extends ? <h2>Logout</h2> : <></>}
+        </li>
+      </Content>
+    </Container>
+  );
+}
--- src/components/Sidebar/style.js
+++ src/components/Sidebar/style.js
@@ -1,1 +1,92 @@
-// to do styles
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: ${({ Extends }) => (Extends ? "15vw" : "5vw")};
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 99;
+
+  @media (max-width: 768px) {
+    width: ${({ Extends }) => (Extends ? "60vw" : "15vw")};
+  }
+  background: rgba(240, 240, 240, 0.8);
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(4.7px);
+  -webkit-backdrop-filter: blur(4.7px);
+  border: 1px solid rgba(240, 240, 240, 0.3);
+`;
+
+export const Content = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  flex-flow: column nowrap;
+  height: 90%;
+  list-style-type: none;
+  padding: 2%;
+
+  li {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-flow: row nowrap;
+    margin: 5% 0%;
+    height: 10%;
+    width: 100%;
+
+    svg {
+      width: ${({ Extends }) => (Extends ? "40%" : "100%")};
+      font-size: 2.5rem;
+    }
+
+    h2 {
+      text-align: left;
+      width: 60%;
+    }
+
+    p {
+      font-size: 1rem;
+    }
+
+    :hover {
+      cursor: pointer;
+      color: var(--color-gray-5);
+    }
+  }
+`;
+
+export const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 10%;
+  white-space: nowrap;
+
+  h1 {
+    font-size: 2.8rem;
+    font-family: "Mukta";
+    color: var(--color-tertiary-3);
+    font-weight: 400;
+
+    ::first-letter {
+      font-weight: 700;
+    }
+
+    span {
+      display: inline-block;
+      color: var(--color-primary-3);
+      font-family: "Mukta";
+      font-weight: 200;
+
+      ::first-letter {
+        font-weight: 700;
+      }
+    }
+  }
+
+  :hover {
+    cursor: pointer;
+  }
+`;
