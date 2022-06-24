649248c864ef03c29fe9a87a9bcda224583e7345
William Baierle <w.baierle@gmail.com>
2022-06-23 21:10:31 -0300

    feat: modal container working

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,1 +1,32 @@
 // To do build component
+import { Container, Header, Content } from "./style";
+import Button from "../Button";
+
+export default function Modal({
+  children,
+  label,
+  modalState,
+  setModalState,
+  ...rest
+}) {
+  const handleCloseModal = (e) => {
+    if (e.target.id === "modal-background") {
+      setModalState(false);
+    }
+    console.log("click");
+  };
+
+  return (
+    <Container id="modal-background" onClick={(e) => handleCloseModal(e)}>
+      <Content>
+        <Header>
+          <h1>Editar {label}</h1>
+          <Button onClick={(e) => setModalState(false)} color="rose">
+            X
+          </Button>
+        </Header>
+        {children}
+      </Content>
+    </Container>
+  );
+}
