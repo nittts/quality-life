25e29b525b9556c92c28a497237a22f6757a2ea6
wiickrz <will.cs.182@gmail.com>
2022-06-27 20:35:24 -0300

    Feat: Search Input

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,1 +1,13 @@
-// To do build component
+import { SearchContainer } from "./style";
+import { FiSearch } from "react-icons/fi";
+
+export default function Search({ children }) {
+  return (
+    <SearchContainer>
+      <input type="text" placeholder="Digite sua busca!"></input>
+      <button>
+        <FiSearch size={24} />
+      </button>
+    </SearchContainer>
+  );
+}
--- src/components/Search/style.js
+++ src/components/Search/style.js
@@ -1,1 +1,45 @@
-// to do styles
+import styled from "styled-components";
+
+export const SearchContainer = styled.div`
+  width: 280px;
+  height: 45px;
+  background-color: #dfdfdf;
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+  font-family: "Mukta";
+
+  input,
+  button {
+    border: none;
+    background: none;
+  }
+
+  input {
+    width: 250px;
+    height: 45px;
+    font-size: 18px;
+    padding-left: 10px;
+    font-weight: 200;
+  }
+
+  input::placeholder {
+    color: var(--color-grey-5);
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+  }
+
+  button:hover {
+    cursor: pointer;
+  }
+
+  input:focus,
+  input::selection,
+  input:active {
+    border: none;
+    outline: none;
+  }
+`;
