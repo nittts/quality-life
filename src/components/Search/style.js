b5cd24f6ceca4fe08eb4af83427b6abbf6c84bcb
Jaqueline Pereira Silva <jaque.perera@gmail.com>
2022-06-29 13:36:30 -0300

    feat/SearchInput hotfix box-shadow

--- src/components/Search/style.js
+++ src/components/Search/style.js
@@ -8,7 +8,8 @@ export const SearchContainer = styled.div`
   display: flex;
   align-items: center;
   font-family: "Mukta";
-  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  //box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  box-shadow: 0 1.5px 4px rgba(0, 0, 0, 0.3);
   backdrop-filter: blur(5px);
   -webkit-backdrop-filter: blur(5px);
   border: 1px solid rgba(217, 217, 217, 0.3);
